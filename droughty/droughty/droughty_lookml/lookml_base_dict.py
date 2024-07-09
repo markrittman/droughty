4801529370499717052ea682679cf733b2e4a672
Mark Rittman <mark.rittman@rittmananalytics.com>
2024-07-09 10:09:41 +0100

    Update lookml_base_dict.py

--- droughty/droughty/droughty_lookml/lookml_base_dict.py
+++ droughty/droughty/droughty_lookml/lookml_base_dict.py
@@ -10,82 +10,82 @@ from collections import defaultdict
 
 from droughty.droughty_core.warehouse_target import create_base_sql
 from droughty.droughty_core.config import (
-    ProjectVariables,
-    get_snowflake_connector_url
+  ProjectVariables,
+  get_snowflake_connector_url
 )
 from droughty.droughty_core.droughty_data_prep import (
-    wrangle_bigquery_dataframes,
-    wrangle_snowflake_dataframes,
-    wrangle_bigquery_dataframes_drill_sets,
-    wrangle_snowflake_dataframes_drill_sets
+  wrangle_bigquery_dataframes,
+  wrangle_snowflake_dataframes,
+  wrangle_bigquery_dataframes_drill_sets,
+  wrangle_snowflake_dataframes_drill_sets
 )
 
 def get_base_dict():
 
-    pd.options.mode.chained_assignment = None
+  pd.options.mode.chained_assignment = None
 
-    warehouse = ProjectVariables.warehouse
+  warehouse = ProjectVariables.warehouse
 
-    sql = create_base_sql()
+  sql = create_base_sql()
 
-    if warehouse == 'big_query':
+  if warehouse == 'big_query':
 
-        credentials = ProjectVariables.service_account
-        project = ProjectVariables.project
+    credentials = ProjectVariables.service_account
+    project = ProjectVariables.project
 
-        dataframe = pandas.read_gbq(sql, dialect='standard', project_id=project, credentials=credentials)
+    dataframe = pandas.read_gbq(sql, dialect='standard', project_id=project, credentials=credentials)
 
-        wrangled_dataframe = wrangle_bigquery_dataframes(dataframe)
+    wrangled_dataframe = wrangle_bigquery_dataframes(dataframe)
 
-    elif warehouse == 'snowflake': 
+  elif warehouse == 'snowflake': 
 
-        engine = create_engine(get_snowflake_connector_url())
+    engine = create_engine(get_snowflake_connector_url())
 
-        connection = engine.connect()
+    connection = engine.connect()
 
-        dataframe = pd.read_sql(sql, connection)
+    dataframe = pd.read_sql(sql, connection)
 
-        wrangled_dataframe = wrangle_snowflake_dataframes(dataframe)
+    wrangled_dataframe = wrangle_snowflake_dataframes(dataframe)
 
-        connection.close()
-        engine.dispose()
+    connection.close()
+    engine.dispose()
 
-    wrangled_dataframe = {n: grp.loc[n].to_dict('index')
-        
-    for n, grp in wrangled_dataframe.set_index(['table_name', 'column_name','data_type','description']).groupby(level='table_name')}
+  wrangled_dataframe = {n: grp.loc[n].to_dict('index')
+     
+  for n, grp in wrangled_dataframe.set_index(['table_name', 'column_name','data_type','description']).groupby(level='table_name')}
 
-    return(wrangled_dataframe)
+  return(wrangled_dataframe)
 
 def get_field_dict():
 
-    pd.options.mode.chained_assignment = None
+  pd.options.mode.chained_assignment = None
 
-    warehouse = ProjectVariables.warehouse
+  warehouse = ProjectVariables.warehouse
 
-    sql = create_base_sql()
+  sql = create_base_sql()
 
-    if warehouse == 'big_query':
+  if warehouse == 'big_query':
 
-        credentials = ProjectVariables.service_account
-        project = ProjectVariables.project
+    credentials = ProjectVariables.service_account
+    project = ProjectVariables.project
 
-        dataframe = pandas.read_gbq(sql, dialect='standard', project_id=project, credentials=credentials)
+    dataframe = pandas.read_gbq(sql, dialect='standard', project_id=project, credentials=credentials)
 
-        wrangled_dataframe = wrangle_bigquery_dataframes_drill_sets(dataframe)
+    wrangled_dataframe = wrangle_bigquery_dataframes_drill_sets(dataframe)
 
-    elif warehouse == 'snowflake': 
+  elif warehouse == 'snowflake': 
 
-        engine = create_engine(get_snowflake_connector_url())
+    engine = create_engine(get_snowflake_connector_url())
 
-        connection = engine.connect()
+    connection = engine.connect()
 
-        dataframe = pd.read_sql(sql, connection)
+    dataframe = pd.read_sql(sql, connection)
 
-        wrangled_dataframe = wrangle_snowflake_dataframes_drill_sets(dataframe)
+    wrangled_dataframe = wrangle_snowflake_dataframes_drill_sets(dataframe)
 
-        connection.close()
-        engine.dispose()
+    connection.close()
+    engine.dispose()
 
-    wrangled_dataframe = wrangled_dataframe.groupby('table_name')['column_name'].apply(list).to_dict()
+  wrangled_dataframe = wrangled_dataframe.groupby('table_name')['column_name'].apply(list).to_dict()
 
-    return(wrangled_dataframe)
+  return(wrangled_dataframe)
